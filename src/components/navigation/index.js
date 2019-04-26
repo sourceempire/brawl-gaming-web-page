e11f4f3f94e25b20626129aee87ea7ac1e36c015
Emil Hallberg <id16ehg@cs.umu.se>
2019-04-26 13:17:06 +0200

    Updated navigation.

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,32 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 import "./navigation.scss";
 
-class Navigation extends Component {
-  render() {
-    return (
-      <div className="navigation">
-        <div className="logo">
-          <img src="/images/logo.png" alt="logo" />
-        </div>
-        <div className="menu">
-          <NavLink exact to="/">
-            Home
-          </NavLink>
-          <NavLink to="/download">Download</NavLink>
-          <NavLink to="/concept">Concept</NavLink>
-          <NavLink to="/games">Games</NavLink>
-          <NavLink to="/faq">FAQ</NavLink>
-          <NavLink to="/login" className="login">
-            Log in
-          </NavLink>
-          <NavLink to="/signup" className="signup">
-            Sign up
-          </NavLink>
-        </div>
+const Navigation = () => {
+  return (
+    <div className="navigation">
+      <div className="logo">
+        <img src="/images/logo.png" alt="logo" />
       </div>
-    );
-  }
-}
+      <div className="menu">
+        <NavLink exact to="/">
+          Home
+        </NavLink>
+        <NavLink to="/download">Download</NavLink>
+        <NavLink to="/concept">Concept</NavLink>
+        <NavLink to="/games">Games</NavLink>
+        <NavLink to="/faq">FAQ</NavLink>
+        <NavLink to="/login" className="login">
+          Log in
+        </NavLink>
+        <NavLink to="/signup" className="signup">
+          Sign up
+        </NavLink>
+      </div>
+    </div>
+  );
+};
 
 export default Navigation;
