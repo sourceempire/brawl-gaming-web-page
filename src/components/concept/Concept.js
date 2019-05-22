174d59a5f01fa003f98c6e544d90adacfe1d280e
samdarrehshourian <id16szn@cs.umu.se>
2019-05-22 16:55:32 +0200

    Started to build the concept site

--- src/components/concept/Concept.js
+++ src/components/concept/Concept.js
@@ -1,12 +1,32 @@
 import React from "react";
 import "./concept.scss";
+import "./concept-components/Grids"
+import Grids from "./concept-components/Grids";
 
 const Concept = () => {
+
+  const concepts = [
+    {
+        left: true,
+        header: "About Brawl Gaming",
+        text: "Textsssss",
+        img: "/images/no_image.svg"
+    },
+    {
+        left: false,
+        header: "About Brawl Gaming 2",
+        text: "Textsssss 2",
+        img: "/images/no_image.svg"
+    }
+]
+
   return (
     <section>
-      <h1>Concept</h1>
+      {concepts.map(concept =>(
+        <Grids concept={concept} />
+      ))}
     </section>
   );
 };
 
-export default Concept;
+export default Concept
