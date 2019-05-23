1f09c1bb67653e581b42f1c88cda0bf84c820ac7
samdarrehshourian <id16szn@cs.umu.se>
2019-05-23 14:42:58 +0200

    Started on side nav component

---
+++ src/components/concept/concept-components/SideNav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../concept.scss";
+
+const SideNav = () => {
+    
+
+    return (
+        <div>
+            <h1>SIDEBAR</h1>
+        </div>
+    )
+}
+  
+export default SideNav;
