2b1b3b7aa7e35f060d96df23c2a9ea54cd9379f3
jakper94 <jakob.j.persson@hotmail.com>
2019-05-27 09:28:06 +0200

    updated section3

--- src/components/home/Section3/Section3.js
+++ src/components/home/Section3/Section3.js
@@ -5,9 +5,9 @@ import GameBox from "../Section3/GameBox/GameBox";
 const Section3 = () => {
   const gameModeText1 = "modes:[{ id:1, mode:'pvp' }, {id:2, mode:'pve'}]";
   const gameArray = [
-    {id:1, title:'CS:GO', image:'game-images/csgo.jpg', modes:[{ id:1, mode:'pvp' }, {id:2, mode:'pve'}] },
-    {id:2, title:'FIFA 19', image:'FIFA-19-BACKGROUND.png',modes:[{ id:1, mode:'Team deathmatch' }, {id:2, mode:'3 vs 3'}] },
-    {id:3, title:'Rocket League', image:'FIFA-19-BACKGROUND.png', modes:[{ id:1, mode:'1 vs 1' }, {id:2, mode:'5 vs 5'}] }
+    {id:1, title:'CS:GO', img_back:'csgo_back.png', img_front:'csgo_front.png', modes:[{ id:1, mode:'pvp' }, {id:2, mode:'pve'}] },
+    {id:2, title:'FIFA 19', img_back:'FIFA-19-BACKGROUND.png',modes:[{ id:1, mode:'Team deathmatch' }, {id:2, mode:'3 vs 3'}] },
+    {id:3, title:'Rocket League', img_back:'FIFA-19-BACKGROUND.png', modes:[{ id:1, mode:'1 vs 1' }, {id:2, mode:'5 vs 5'}] }
     ]
   return (
     <div>
@@ -20,7 +20,7 @@ const Section3 = () => {
         {gameArray.map(game=>(
           <GameBox
           label={game.title}
-          image={game.image}
+          image={game.img_back}
           gamemodes={game.modes}
 
         />
