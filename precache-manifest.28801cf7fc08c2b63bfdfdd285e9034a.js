d93896010d4c0f19bb219a60f6dc69aab8d90593
Spunkten <martin.a.willman@gmail.com>
2019-09-12 14:05:03 +0200

    Updates

---
+++ precache-manifest.28801cf7fc08c2b63bfdfdd285e9034a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "01d1bca26034da4eedc9",
+    "url": "/static/css/main.1e347680.chunk.css"
+  },
+  {
+    "revision": "01d1bca26034da4eedc9",
+    "url": "/static/js/main.064df449.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5da4d87d4ef37a3807d0",
+    "url": "/static/js/2.389c4193.chunk.js"
+  },
+  {
+    "revision": "43396d12e0ee8a76f6f59796809c1e01",
+    "url": "/index.html"
+  }
+];
